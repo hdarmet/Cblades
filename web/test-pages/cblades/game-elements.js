3baa1608a364fb22801782f2967b628c605aa7e7
henri.darmet <henri.darmet@viseo.com>
2023-11-13 10:56:11 +0100

    everything for fire playing

---
+++ web/test-pages/cblades/game-elements.js
@@ -0,0 +1,22 @@
+'use strict'
+
+export let banner = {
+    name: "banner",
+    path: "./../units/banner.png"
+}
+
+export let banner1 = {
+    name: "banner1",
+    path: "./../units/banner1.png"
+}
+
+export let banner2 = {
+    name: "banner2",
+    path: "./../units/banner2.png"
+}
+
+export let banner3 = {
+    name: "banner3",
+    path: "./../units/banner3.png"
+}
+
